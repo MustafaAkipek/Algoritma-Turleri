2de1dd33687babbe9ca020f6703c9defce28a758
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-08-31 15:12:00 +0300

    code

---
+++ Yigin Siralaması(Heap Sort)/heapsort.py
@@ -0,0 +1,32 @@
+def max_heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+        
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+        
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        max_heapify(arr, n, largest)
+
+def build_max_heap(arr):
+    n = len(arr)
+    for i in range(n // 2 - 1, -1, -1):
+        max_heapify(arr, n, i)
+
+def max_heap_sort(arr):
+    n = len(arr)
+    build_max_heap(arr)
+    
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        max_heapify(arr, i, 0)
+
+# Örnek kullanım
+arr = [4, 10, 3, 5, 1]
+max_heap_sort(arr)
+print("Sıralanmış dizi:", arr)
